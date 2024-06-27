12c73cb05316b1c2c7cca62d10f3a62a9ed78c7e
Andrew6201 <okekechiemerie2021@gmail.com>
2024-06-27 02:06:22 +0100

    updated now

--- blengoservices/settings.py
+++ blengoservices/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xftwzyz*q)cz!^jgn%2dmjn0s$r_*gb&khld!zt9_e62p9$q6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
